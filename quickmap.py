3f790091275dcb1c3132f414c70581f2f9d27891
Recolic Keghart <root@recolic.net>
2017-12-05 13:30:30 +0800

    Add inverse

--- quickmap.py
+++ quickmap.py
@@ -17,10 +17,11 @@ def dotMultiply(vctA, vctB):
         ans += a * b
     return ans
 
-def GetMap(parrX, parrY, windowSizeX=12, windowSizeY=8, extendXRate=1, line=False, passO=False, maxXPower=1):
+def GetMap(parrX, parrY, windowSizeX=12, windowSizeY=8, extendXRate=1, extendYRate=1, line=False, passO=False, maxXPower=1, inverseK=False):
     print('Generic-GetMap by Recolic.')
     arrX, arrY = parrX, parrY
-    maxX = max(arrX) * extendXRate
+    maxX, maxY = max(arrX) * extendXRate, max(arrY) * extendYRate
+    minX, minY = min(arrX) * extendXRate, min(arrY) * extendYRate
     # Do calculate
     # y = [k0 k1 k2 ...] dot [x^0 x^1 x^2 ...]
     print('Your input: ', arrX, '|', arrY)
@@ -33,14 +34,18 @@ def GetMap(parrX, parrY, windowSizeX=12, windowSizeY=8, extendXRate=1, line=Fals
             vctX[0] = 0
         return dotMultiply(k, vctX)
 
-    def lossFunc(k, x, y): return lineFunc(k, x) - y
+    def lossFunc(k, x, y): return abs(lineFunc(k, x) - y)
 
     # Fire!
     if line:
         kInit = [1 for _ in range(maxXPower + 1)]
         kInit[0] = 0 # guarantee passO.
-        kFinal, _ = leastsq(lossFunc, kInit, args=(X, Y))
-        print('Fit line done. k=', kFinal)
+        if inverseK:
+            kFinal, _ = leastsq(lossFunc, kInit, args=(Y, X))
+            print('Fit line done. k^-1=', kFinal)
+        else:
+            kFinal, _ = leastsq(lossFunc, kInit, args=(X, Y))
+            print('Fit line done. k=', kFinal)
     else:
         print('Drawing map without fitting a line...')
 
@@ -51,8 +56,12 @@ def GetMap(parrX, parrY, windowSizeX=12, windowSizeY=8, extendXRate=1, line=Fals
     plt.figure(figsize=(windowSizeX, windowSizeY))
     plt.scatter(X, Y, color="red", label="Sample Point", linewidth=3)
     if line:
-        px = numpy.linspace(0, maxX, 1000)
-        py = dotMultiply(kFinal, [px ** power for power in range(maxXPower + 1)])
+        if inverseK:
+            py = numpy.linspace(minY, maxY, 1000)
+            px = dotMultiply(kFinal, [py ** power for power in range(maxXPower + 1)])
+        else:
+            px = numpy.linspace(minX, maxX, 1000)
+            py = dotMultiply(kFinal, [px ** power for power in range(maxXPower + 1)])
         plt.plot(px, py, color="orange", label="Fitting Line", linewidth=2)
     plt.legend()
     plt.grid()
