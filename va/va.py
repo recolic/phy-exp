6d36a331a5b1f2da8555f771ae65e2ba8396a7ef
Recolic Keghart <root@recolic.net>
2018-04-18 14:10:29 +0800

    fix va.

--- va/va.py
+++ va/va.py
@@ -1,7 +1,9 @@
 #!/bin/env python3
 import numpy
+
 import sys
-from quickmap import GetMap as draw
+sys.path.append('..')
+from quickmap import *
 
 print('Usage: python va.py path/va-dat.in <Voltmeter range(V)> <Ammeter range(mA)> <fit line>')
 print('Example: python3 va.py va-dat.in 1 15 true')
@@ -10,6 +12,6 @@ Uraw,Iraw=dat[:,0],dat[:,1]
 U=[utrue/100*float(sys.argv[2]) for utrue in Uraw]
 I=[itrue/150*float(sys.argv[3]) for itrue in Iraw]
 if sys.argv[4] == 'true':
-    draw(U,I,6,4,line=True,passO=True)
+    GetMap(U,I,polyLine=True,poly_passO=True)
 else:
-    draw(U,I,6,4)
+    GetMap(U,I)
